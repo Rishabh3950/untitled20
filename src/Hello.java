7c91d9e867ab7b6a880fde5bd1dd1d521aa97a3f
Hp <rishabh.yadav_cs18@gla.ac.in>
2021-02-09 22:04:01 +0530

    add number

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    public static void main(String args[]){
+        int a=2;
+        int b=4;
+        int c=5;
+        int sum= a +  b +  c;
+        System.out.println(sum);
+
+    }
+
+}
